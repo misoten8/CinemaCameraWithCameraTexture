e56fb2990fad83ee3db6bff84264f4459f8ae31d
WIN8THS40779\Ando Hiroki <ashsavior17@gmail.com>
2017-10-24 16:15:07 +0900

    cinemachineの画面をレンダーテクスチャに変換

---
+++ Assets/querychandemo1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class querychandemo1 : MonoBehaviour {
+    private Animator animator;
+    private bool Active;//updateの有効化on/off
+                        // Use this for initialization
+    void Start () {
+        Active = true;
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Active == true && Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.08f;
+            animator.SetBool("isrunning", true);
+        }
+        else
+        {
+            animator.SetBool("isrunning", false);
+        }
+        if (Active == true && Input.GetKey("right"))
+        {
+            transform.Rotate(0, 2, 0);
+        }
+        if (Active == true && Input.GetKey("left"))
+        {
+            transform.Rotate(0, -2, 0);
+        }
+    }
+}
